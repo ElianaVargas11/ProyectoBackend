3f4c2a7266c5c472b73586d8c8294851e35b503f
Eliana Vargas Rojas <evargasrojas11@gmail.com>
2021-06-18 00:52:39 -0400

    primer commit

---
+++ src/test/java/bo/edu/ucb/ingsoft/ProyectoBackend/ProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.ProyectoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
